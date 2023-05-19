992aecbf0d2821b9cebf3996582364df229795bd
puneet sharma <132386795+rockingboi@users.noreply.github.com>
2023-05-19 23:13:54 +0530

    Add files via upload

---
+++ count_of_charrecters.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAX_SIZE 100 
+int main()
+{
+    char str[MAX_SIZE];
+    int i;
+
+  
+    printf("Enter your text : ");
+    gets(str);
+
+    for(i=0; str[i]!='\0'; i++)
+    {
+       
+        if(str[i]>='a' && str[i]<='z')
+        {
+            str[i] = str[i] - 32;
+        }
+    }
+
+    printf("Uppercase string : %s",str);
+    return 0;
+}s %d\n",k);
+    printf("alphabet are %d\n",c);
+    printf("space are %d\n",f);
+    printf("special character are %d\n",k-(c+f+d)-1);
+    return 0;
+}
---
+++ heart.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    for(int i=1;i<=t;i++)
+    {
+        int a,b,c;
+        scanf("%d %d %d",&a,&b,&c);
+        int sum = a+b+c;
+        if(sum==180&&a!=0&&b!=0&&c!=0)
+        {
+            printf("Valid triangle\n");
+        }
+        else
+        {
+            printf("Not a valid triangle\n");
+        }
+    }
+    return 0;
+}    printf("*");
+        }
+
+        printf("\n");
+    }
+
+    for(i=n; i>=1; i--)
+    {
+        for(j=i; j<n; j++)
+        {
+            printf(" ");
+        }
+
+        for(j=1; j<=(i*2)-1; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
